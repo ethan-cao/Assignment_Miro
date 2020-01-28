81c7aca5e967f84b569aae5dbfa8676622ad0ca7
ethan-cao <ethancaocgx@gmail.com>
2020-01-28 13:51:58 +0000

    setup

---
+++ src/emails-editor.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const EmailsEditor = ({container, ...options}) => {
+    console.log(`Initialization -- Render EmailsEditor in ${container.id} with options ${JSON.stringify(options)}`);
+
+
+
+    console.log(`Initialization -- Finishing rendering`);
+};
+
+module.exports = EmailsEditor;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	entry: "./src/emails-editor.js",
+	output: {
+		filename: "emails-editor.js",
+		path: path.resolve(__dirname, "dist"),
+		library: "EmailsEditor"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					process.env.NODE_ENV !== "production"
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader"
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].min.css",
+			chunkFilename: "[id].min.css"
+		})
+	],
+	devtool: "inline-source-map"
+};
