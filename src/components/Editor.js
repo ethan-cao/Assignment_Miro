48581361b064b281c871f680600c5173c1069e96
ethan-cao <ethancaocgx@gmail.com>
2020-01-29 17:01:17 +0000

    input

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const EDITOR_CLASS = "form__editor";
+const INPUT_CLASS = "form__editor__input";
+const EMAIL_CLASS = "form__editor__email";
 
 const Editor = () => {
 	console.log(`Build -- [Editor] -- start`);
 
     const template = `
         <div class=${EDITOR_CLASS}>
+            <input class=${INPUT_CLASS} type="text" placeholder="add more people...">
         </div>
     `;
 
