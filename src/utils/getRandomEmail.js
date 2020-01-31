86216b5ca277341f09cd305ef96b79ca3f000bc1
ethan-cao <ethancaocgx@gmail.com>
2020-01-31 19:24:34 +0000

    update

--- src/utils/getRandomEmail.js
+++ src/utils/getRandomEmail.js
@@ -1,8 +1,15 @@
 "use strict";
 
+const getRandomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+
+	return Math.floor(Math.random() * (max - min)) + min;
+};
+
 const getRandomEmail = () => {
-    const chars = "abcdefghijklmnopqrstuvwxyz1234567890";
-    const accountLength = 6;
+	const chars = "abcdefghijklmnopqrstuvwxyz";
+	const accountLength = getRandomInt(4, 10);
 
 	let account = "";
 
