7f69de39f560e17012937e9f2a6bf7e57d943319
ethan-cao <ethancaocgx@gmail.com>
2020-02-01 10:54:28 +0000

    comments added

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import "./styles/main.scss";
 import Form from "./components/Form";
 import Listener from "./events/Listener";
 
-// TODO log time spent
-
+/**
+ * EmailsEditor render a form based, in which user can type email address.
+ * 
+ * @param {Object} specification - specification used to describe where the form should be rendered and what to be displayed, 
+ * 								   including name of the board and emails shown in the editor
+ * @return {Object} - form object, including the form DOM object and API to get and set emails, as well as to subscribe to changes
+ */
 const EmailsEditor = ({ container, ...options }) => {
 	console.log(`Render -- [EmailsEditor] -- Render in ${container.id} with options ${JSON.stringify(options)}`);
+
+	const start = performance.now();
 
 	const { form, getEmails, setEmails, subscribe } = Form(options);
     container.append(form);
     
     Listener.listenOn(form, ["click", "keydown", "focusout", "paste"], {setEmails, getEmails});
 
-	console.log(`Render -- [EmailsEditor] -- Finishing rendering`);
+	const end = performance.now();
+
+	console.log(`Render -- [EmailsEditor] -- Finishing rendering #${options.boardName}# within ${Math.ceil(end - start)} ms`);
 
 	return {
 		getEmails,
